a4e4d0e87476ea0f1bc20e6d4030fcfae3a6ebe9
ColourfulRhythm <119403527+ColourfulRhythm@users.noreply.github.com>
2025-09-19 16:59:53 +0100

    Supabase: add Allow-Methods/Max-Age to CORS

--- supabase/functions/generate-docs-preview/index.ts
+++ supabase/functions/generate-docs-preview/index.ts
@@ -78,6 +78,8 @@ serve(async (req) => {
 		const corsHeaders = {
 			"Access-Control-Allow-Origin": "*",
 			"Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
+			"Access-Control-Allow-Methods": "POST, OPTIONS",
+			"Access-Control-Max-Age": "86400",
 		};
 		if (req.method === "OPTIONS") {
 			return new Response("ok", { headers: corsHeaders });
